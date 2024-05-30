3af0838d534f5f5aa48a4a232833dde383c45cbf
hjulstromandre <hjulstromandre@gmail.com>
2024-05-30 15:38:57 +0300

    lägger till ett jämnt fördelat innehav samt jämfört med MSFT

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ historical_data = yf.download(ticker_symbols, start="2004-01-01", end="2024-01-0
 
 close_prices = historical_data['Close']
 
+num_stocks = len(ticker_symbols)
+equal_weight = 1 / num_stocks
+portfolio_value = close_prices.apply(lambda x: x * equal_weight, axis=1).sum(axis=1)
+msft_prices = close_prices['MSFT']
 
-fig = px.line(close_prices, title='Historical Closing Prices of Large-Cap Stocks')
-fig.update_layout(xaxis_title='Date', yaxis_title='Price (USD)', legend_title='Ticker Symbols')
+fig = px.line()
+fig.add_scatter(x=portfolio_value.index, y=portfolio_value, name='Equally-Weighted Portfolio')
+fig.add_scatter(x=msft_prices.index, y=msft_prices, name='MSFT Stock')
+fig.update_layout(title='Comparison of Equally-Weighted Portfolio and MSFT Stock',
+                  xaxis_title='Date', yaxis_title='Price (USD)')
 fig.show()
